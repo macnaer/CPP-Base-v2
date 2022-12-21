37dd2f9cd295e4da530445b6eba3aa8fe41462a1
macnaer <trofimchuk.an@gmail.com>
2022-12-21 10:53:45 +0200

    Added ASCII example

--- 04.Switch/Source.cpp
+++ 04.Switch/Source.cpp
@@ -3,19 +3,39 @@
 using namespace std;
 
 void main() {
-	short seasson = 0;
-	cout << "Enter seasson: " << endl;
-	cin >> seasson;
 
-	switch (seasson)
-	{
-		case 1: cout << "Winter" << endl; break;
-		case 2: cout << "Spring" << endl; break;
-		case 3: cout << "Summer" << endl; break;
-		case 4: cout << "Autumn" << endl; break;
-		//case 'y':  cout << "You press y" << endl; break;
-		default:
-			cout << "Wring choise. Enter 1-4 for seassons" << endl;
-			break;
-	}
+	//short seasson = 0;
+	//cout << "Enter seasson: " << endl;
+	//cin >> seasson;
+
+	//switch (seasson)
+	//{
+	//	case 1: cout << "Winter" << endl; break;
+	//	case 2: cout << "Spring" << endl; break;
+	//	case 3: cout << "Summer" << endl; break;
+	//	case 4: cout << "Autumn" << endl; break;
+	//	//case 'y':  cout << "You press y" << endl; break;
+	//	default:
+	//		cout << "Wring choise. Enter 1-4 for seassons" << endl;
+	//		break;
+	//}
+
+
+	/*const int HOURS = 10;
+	cout << "Hours " << HOURS << endl;*/
+
+	/*bool exit = true;
+
+	exit == true ? cout << "Exit = true" : cout << "Exit = false" << endl;*/
+
+	char value = 'a';
+	cout << "Value = " << value << " Code = " << int(value) << endl;
+
+	int number = 42;
+	cout << "Number = " << number << " Code = " << char(number) << endl;
+
+	// https://en.cppreference.com/w/cpp/language/ascii
+
+
+
 }
