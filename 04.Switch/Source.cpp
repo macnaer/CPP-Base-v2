32821df8df074f891790d5fb43e32655f33efd2c
macnaer <trofimchuk.an@gmail.com>
2022-12-23 09:28:14 +0200

    Start witch loops

--- 04.Switch/Source.cpp
+++ 04.Switch/Source.cpp
@@ -4,35 +4,41 @@ using namespace std;
 
 void main() {
 
-	//short seasson = 0;
-	//cout << "Enter seasson: " << endl;
-	//cin >> seasson;
+	short seasson = 0;
+	cout << "Enter seasson: " << endl;
+	cin >> seasson;
 
-	//switch (seasson)
-	//{
-	//	case 1: cout << "Winter" << endl; break;
-	//	case 2: cout << "Spring" << endl; break;
-	//	case 3: cout << "Summer" << endl; break;
-	//	case 4: cout << "Autumn" << endl; break;
-	//	//case 'y':  cout << "You press y" << endl; break;
-	//	default:
-	//		cout << "Wring choise. Enter 1-4 for seassons" << endl;
-	//		break;
-	//}
+	switch (seasson)
+	{
+		case 1: cout << "Winter" << endl; break;
+		case 2: cout << "Spring" << endl; break;
+		case 3: cout << "Summer" << endl; break;
+		case 4: cout << "Autumn" << endl; break;
+		//case 'y':  cout << "You press y" << endl; break;
+		default:
+			cout << "Wring choise. Enter 1-4 for seassons" << endl;
+			break;
+	}
 
 
 	/*const int HOURS = 10;
 	cout << "Hours " << HOURS << endl;*/
 
-	/*bool exit = true;
+	//bool exit = false;
+	//int choise = 0;
+	//cout << "1.Add book...... 0. Exit" << endl;
+	//cin >> choise;
+	/*if (choise == 0) {
+		exit = true;
+	}*/
 
-	exit == true ? cout << "Exit = true" : cout << "Exit = false" << endl;*/
+	/*choise == 0 ? cout << "Exit = true" : cout << "Exit = false" << endl;*/
 
-	char value = 'a';
+	/*char value = 'a';
 	cout << "Value = " << value << " Code = " << int(value) << endl;
 
 	int number = 42;
-	cout << "Number = " << number << " Code = " << char(number) << endl;
+	cout << "Number = " << number << " Code = " << char(number) << endl;*/
 
 	// https://en.cppreference.com/w/cpp/language/ascii
 
---
+++ 05. Loops/Source.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void main() {
+
+	//int iterator = 10;
+
+	//while (iterator != 0) {
+	//	cout << "While.... " << iterator << endl;
+	//	/*iterator++;*/
+	//	iterator--;
+	//}
+
+	int iterator = 10;
+
+	do {
+		cout << "do while ..." << iterator << endl;
+		iterator--;
+	} while (iterator != 0);
+	
+}
