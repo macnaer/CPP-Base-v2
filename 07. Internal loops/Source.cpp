587b17c46c7dd36bb14231ae35e5f32dda436781
macnaer <trofimchuk.an@gmail.com>
2023-01-16 11:35:26 +0200

    Piramid

--- 07. Internal loops/Source.cpp
+++ 07. Internal loops/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void main() {
 
-	int row = 10;
+	/*int row = 10;
 	int col = 10;
 
 	for (int i = 0; i < row; i++)
@@ -19,5 +19,38 @@ void main() {
 
 		}
 		cout << endl;
+	}*/
+
+	/*int row = 5;
+	int col = 1;*/
+
+	/*for (int i = 1; i < row; i++)
+	{
+		for (int j = 10; j > col; j--)
+		{
+			cout << "i => " << i << " ";
+			cout << "j => " << j << endl;
+			 
+		}
+		cout << endl;
+	}*/
+
+	int rows, i, j, space;
+
+	cout << "Enter number of rows: ";
+	cin >> rows;
+
+	for (i = 1; i <= rows; i++)
+	{
+		for (space = i; space < rows; space++)
+		{
+			cout << " ";
+		}
+		for (j = 1; j <= (2 * i - 1); j++)
+		{
+			cout << "*";
+		}
+
+		cout << endl;;
 	}
 }
