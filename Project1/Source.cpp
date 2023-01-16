0aba1b4a56da46fe67c0befba70f5327b6173f01
macnaer <trofimchuk.an@gmail.com>
2023-01-16 10:00:33 +0200

    internal for

---
+++ Project1/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void main() {
+
+	int row = 10;
+	int col = 10;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++) {
+			if (i % 2 == 0) {
+				cout << "* ";
+			}
+			else {
+				cout << "# ";
+			}
+			
+		}
+		cout << endl;
+	}
+}
