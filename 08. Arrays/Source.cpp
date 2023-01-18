16df802dda02d62a734962959e1f3c4b2ec8c950
macnaer <trofimchuk.an@gmail.com>
2023-01-18 11:44:18 +0200

    Example

--- 08. Arrays/Source.cpp
+++ 08. Arrays/Source.cpp
@@ -29,58 +29,99 @@ void main() {
 	//}
 
 
-	const int SIZE = 7;
+	//const int SIZE = 7;
 
-	int arr[7];
+	//int arr[7];
 
-	int plusCouner = 0, minusCounter = 0;
+	//int plusCouner = 0, minusCounter = 0;
 
-	for (int i = 0; i < SIZE; i++)
-	{
-		arr[i] = -12 + rand() % 50;
-	}
+	//for (int i = 0; i < SIZE; i++)
+	//{
+	//	arr[i] = -12 + rand() % 50;
+	//}
 
-	cout << "=============== Print array================" << endl;
-
-
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << "Arr [" << i << "] = " << arr[i] << endl;
-		/*if (arr[i] > 0) {
-			plusCouner++;
-		}
-		else if (arr[i] < 0) {
-			minusCounter++;
-		}*/
-	}
-
-	int max = -13;
-	int min = 100;
-	int maxIndex = 0;
-
-	for (int i = 0; i < SIZE; i++)
-	{
-		if (max < arr[i]) {
-			max = arr[i];
-			maxIndex = i;
-		}
-	}
+	//cout << "=============== Print array================" << endl;
 
 
-	cout << "=============================" << endl;
-	for (int i = 0; i < SIZE; i++)
-	{
-		cout << "Arr [" << i << "] = " << arr[i] << endl;
-		/*if (arr[i] > 0) {
-			plusCouner++;
-		}
-		else if (arr[i] < 0) {
-			minusCounter++;
-		}*/
-	}
 
-	cout << "maxIndex = " << maxIndex << endl;
+	//for (int i = 0; i < SIZE; i++)
+	//{
+	//	cout << "Arr [" << i << "] = " << arr[i] << endl;
+	//	/*if (arr[i] > 0) {
+	//		plusCouner++;
+	//	}
+	//	else if (arr[i] < 0) {
+	//		minusCounter++;
+	//	}*/
+	//}
+
+	//int max = -13;
+	//int min = 100;
+	//int maxIndex = 0;
+
+	//for (int i = 0; i < SIZE; i++)
+	//{
+	//	if (max < arr[i]) {
+	//		max = arr[i];
+	//		maxIndex = i;
+	//	}
+	//}
+
+
+	//cout << "=============================" << endl;
+	//for (int i = 0; i < SIZE; i++)
+	//{
+	//	cout << "Arr [" << i << "] = " << arr[i] << endl;
+	//	/*if (arr[i] > 0) {
+	//		plusCouner++;
+	//	}
+	//	else if (arr[i] < 0) {
+	//		minusCounter++;
+	//	}*/
+	//}
+
+	//cout << "maxIndex = " << maxIndex << endl;
 	//cout << "More then 0 = " << plusCouner << endl;
 	//cout << "Less then 0 = " << minusCounter << endl;
+
+	const int SIZE = 10;
+	int arr[SIZE];
+	int minValue = 100, minIndex = 0, maxValue = -100, maxIndex = 0, tmp = 0;;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = -100 + rand() % 200;
+	}
+
+	cout << "======================Print=================" << endl;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << "arr [" << i << "] = " << arr[i] << endl;
+		if (maxValue < arr[i]) {
+			maxValue = arr[i];
+			maxIndex = i;
+		}
+		if (minValue > arr[i]) {
+			minValue = arr[i];
+			minIndex = i;
+		}
+	}
+
+	cout << "==========================" << endl;
+	cout << "Max value = " << maxValue << " index = " << maxIndex << endl;;
+	cout << "Min value = " << minValue << " index = " << minIndex << endl;;
+
+
+	tmp = minValue;
+	arr[minIndex] = maxValue;
+	arr[maxIndex] = tmp;
+
+	
+	cout << "===========After changing values============" << endl;
+	for (int i = 0; i < SIZE; i++)
+	{	
+		cout << "arr [" << i << "] = " << arr[i] << endl;
+	
+	}
 }
