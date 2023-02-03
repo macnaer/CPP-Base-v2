cb681b33e05e8d863c3b3ea960d70a86f958bc3b
macnaer <trofimchuk.an@gmail.com>
2023-02-03 10:21:10 +0200

    Add example

--- 10. functions/Source.cpp
+++ 10. functions/Source.cpp
@@ -20,14 +20,42 @@ int Plus(int a, int b) {
 	// Error
 }
 
+
+char Compare(int a, int b) {
+	char result;
+	if (a > b) {
+		result = '>';
+	}
+	else if (a < b) {
+		result = '<';
+	}
+	else {
+		result = '=';
+	}
+
+	return result;
+}
+
 void main() {
 
 	/*Hello();*/
 	/*Plus(5, 10);*/
-	int result = 0;
+	/*int result = 0;
 	result = Plus(3, 5);
 
 	cout << "Result Value: " << result << " Result size: "
-				<< sizeof(result) << " Result address: " << &result << endl;
+				<< sizeof(result) << " Result address: " << &result << endl;*/
+
+	int a = 0, b = 0;
+	char sign;
+
+	cout << "Enter first digit: ";
+	cin >> a;
+
+	cout << "Enter second digit: ";
+	cin >> b;
+
+	sign = Compare(a, b);
+	cout << "A " << sign << " B" << endl;
 }
 
