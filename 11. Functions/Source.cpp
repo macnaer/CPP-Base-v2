b9287905b8e56964686ab6b0550361fa798f1540
macnaer <trofimchuk.an@gmail.com>
2023-02-06 11:24:03 +0200

    Add example

--- 11. Functions/Source.cpp
+++ 11. Functions/Source.cpp
@@ -17,13 +17,43 @@ void Print(int arr[], const int SIZE) {
 	}
 }
 
+
+void CopyTo(int sourceArr[], int destinationArr[], const int SIZE) {
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		destinationArr[i] = sourceArr[i];
+	}
+}
+
+void ExtendArray(int arrA[], int arrB[], int arrC[], const int SIZE) 
+{
+	int iterator = 0;
+	for (int i = 0; i < (SIZE * 2); i++)
+	{
+		if (i < SIZE) {
+			arrC[i] = arrA[i];
+		}
+		else {
+			arrC[i] = arrB[iterator];
+			iterator++;
+		}
+	}
+}
+
 void main() {
 
 	srand((unsigned)time(NULL));
 
-	const int SIZE = 10;
-	int arr[SIZE];
+	const int SIZE = 5;
+	int arrA[SIZE];
+	int arrB[SIZE];
+	int arrC[SIZE * 2];
 
-	Fill(arr, SIZE);
-	Print(arr, SIZE);
+	Fill(arrA, SIZE);
+	Fill(arrB, SIZE);
+	ExtendArray(arrA, arrB, arrC, SIZE);
+	Print(arrC, SIZE * 2);
+	/*CopyTo(arrA, arrB, SIZE);*/
+	/*Print(arrB, SIZE);*/
 }
