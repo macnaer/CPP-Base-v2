6f125d199fb422b981187935035a87330e28daa7
macnaer <trofimchuk.an@gmail.com>
2023-02-22 11:20:22 +0200

    Arr

--- 16. Pointers intro/Source.cpp
+++ 16. Pointers intro/Source.cpp
@@ -8,18 +8,49 @@ void Counter(int *b) {
 	/**b = *b + 1;*/
 }
 
+void Fill(int pArr[], const int SIZE) {
+	for (int i = 0; i < SIZE; i++) {
+		pArr[i] = i;
+	}
+}
+
+void Print(int pArr[], const int SIZE) {
+	for (int i = 0; i < SIZE; i++) {
+		cout << "arr[" << i << "] = " << pArr[i] << endl;
+	}
+}
+
 void main() {
 
-	int a = 5;
-	/*cout << "a value: " << a << " a address: " << &a << endl;*/
+	//int a = 5;
+	///*cout << "a value: " << a << " a address: " << &a << endl;*/
 
-	int* b;
-	
-	/*b = &a;*/
-	b = &a;
-	/*cout << "b value: " << *b << " b address: " << &b << endl;*/
+	//int* p = nullptr;
 
-	Counter(b);
-	cout << "b value: " << *b << " b address: " << b << endl;
+	//
+	///*b = &a;*/
+	//p = &a;
+	///*cout << "b value: " << *b << " b address: " << &b << endl;*/
 
+	//Counter(p);
+	//cout << "b value: " << *p << " b address: " << p << endl;
+
+	//int b = 10;
+	//double c = 4.5;
+	//void* pB = nullptr;
+
+	//pB = &b;
+	//cout << "Value: " << &pB << endl;
+
+	//pB = &c;
+	//cout << "Value: " << &pB << endl;
+
+	const int SIZE = 10;
+	int arr[SIZE];
+	cout << "Before arr address " << arr << endl;
+
+	int* pArr = nullptr;
+	pArr = &arr[0];
+	Fill(pArr, SIZE);
+	Print(pArr, SIZE);
 }
