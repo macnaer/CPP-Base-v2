2058bf53135e50bdad728a313e3c6c4ec07fefa5
macnaer <trofimchuk.an@gmail.com>
2023-04-03 12:03:55 +0300

    File

--- 21. Files/Source.cpp
+++ 21. Files/Source.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 
 void main() {
-	
-	string path = "lib/db.txt";
-	ofstream writeFile;
-	string text;
+
+	/*string path = "lib/db.txt";*/
+	//ofstream writeFile;
+	//string text;
 	//============Write to file==============
 	//writeFile.open(path, ofstream::app);
 	//if (!writeFile.is_open()) {
@@ -23,39 +23,40 @@ void main() {
 	//}
 	//writeFile.close();
 	//============Read from file==============
+	string path = "lib/db.txt";
 	ifstream readFile;
-	readFile.open(path);
-	//if (!readFile.is_open()) {
-	//	cout << "Can't open file!" << endl;
-	//}
-	//else {
-	//	cout << "Opened!" << endl;
-	//	char data;
-	//	while (readFile.get(data)) {
-	//		cout << data;
-	//	}
-	//}
-	//readFile.close();
-
-
 	string arr[4];
-	int i = 0;
+	int i = 0, j = 0;
 	string data;
-	while (!readFile.eof()) {
-		getline(readFile, data);
-		for (int i = 0; i < data.length(); i++) {
-			arr[i] = data[i];
 
-			if ((int)data[i] == 124) {
-				cout << "|" << endl;
-			}
-		}	
-		i++;
+	readFile.open(path);
+	if (!readFile.is_open()) {
+		cout << "Can't open file!" << endl;
 	}
-	readFile.close();
+	else {
+		cout << "Opened!" << endl;
+		while (!readFile.eof())
+		{
+			getline(readFile, data);
+			for (int i = 0; i < data.length(); i++)
+			{
+				if ((int)data[i] != 124)
+				{
+					arr[j] += data[i];
+				}
+				else
+				{
+					j++;
+				}
+			}
+		}
+		readFile.close();
 
-	/*for (int i = 0; i < 4; i++) {
-		cout << arr[i];
-	}*/
+		for (int i = 0; i < 4; i++) {
+			cout << arr[i] << " " << endl;
+		}
+	}
+	
 
+	
 }
